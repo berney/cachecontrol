1c043d7846ee0c223100674d845d7d92a310bfd4
Sam Collinson <smc@affinity.net.nz>
2016-01-30 23:30:04 +1100

    working impl

--- cachecontrol/caches/sqlalchemy_cache.py
+++ cachecontrol/caches/sqlalchemy_cache.py
@@ -1,25 +1,30 @@
 from ..cache import BaseCache
 
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, LargeBinary
 
 
 class SQLAlchemyCacheMixin(object):
     cache_key = Column(String, nullable=False, unique=True)
-    cache_value = Column(String)
+    cache_value = Column(LargeBinary)
 
 
 class SQLAlchemyCache(BaseCache):
     def __init__(self, session, klass):
         self.session = session
 
-        if isinstance(klass, SQLAlchemyCacheMixin):
+        if issubclass(klass, SQLAlchemyCacheMixin):
             self.klass = klass
         else:
             raise ValueError('klass must inherit SQLAlchemyCacheMixin')
 
     def get(self, cache_key):
-        return self.session.query(self.klass).\
+        obj = self.session.query(self.klass).\
             filter(self.klass.cache_key == cache_key).one_or_none()
+
+        if obj is not None:
+            return obj.cache_value
+
+        return None
 
     def set(self, cache_key, cache_value):
         obj = self.get(cache_key)
